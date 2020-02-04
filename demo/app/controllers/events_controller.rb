415cc162b7e5d1bc9dd68aa44f87a36c78a904a6
jeffchao98 <philadelphiafs@gmail.com>
2020-02-04 16:30:39 +0900

    MISC - Validation feedback

--- demo/app/controllers/events_controller.rb
+++ demo/app/controllers/events_controller.rb
@@ -8,17 +8,22 @@ class EventsController < ApplicationController
     end
     def create
         @event = Event.new(event_params)
-        @event.save
-      
-        redirect_to :action => :index
+        if @event.save
+            redirect_to :action => :index
+        else
+            render :action => :new
+        end
     end
     def show
     end
     def edit
     end
     def update
-        @event.update(event_params)
-        redirect_to :action => :show, :id => @event
+        if @event.update(event_params)
+            redirect_to :action => :show, :id => @event
+        else
+            render :action => :edit
+        end
     end
     def destroy
         @event.destroy
