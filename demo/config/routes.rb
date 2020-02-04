3bd20a558ba30017c3fcbb1e5349a0c1f4536046
jeffchao98 <philadelphiafs@gmail.com>
2020-02-04 15:09:38 +0900

    MISC - wild card route

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "welcome" => "welcome#index"
 
   root :to => "welcome#index"
+  match ':controller(/:action(/:id(.:format)))', :via => :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
